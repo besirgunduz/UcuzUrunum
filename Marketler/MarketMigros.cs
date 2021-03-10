093c7973bac54205a766fe9ddc6571acfcde9edd
besirgunduz <besirgunduz1993@gmail.com>
2021-03-11 00:33:35 +0300

    Update 11.03.2021

---
+++ Marketler/MarketMigros.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnUcuzUrun.Marketler
+{
+    public class MarketMigros:Market
+    {
+        public MarketMigros()
+        {
+            Name
+        }
+    }
+}
